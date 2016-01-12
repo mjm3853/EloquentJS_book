a792932b93b5722ec2d6aa50946f95f5942ce068
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-01-11 22:43:46 -0500

    more comments on the parser, and added the evaluate function

--- ch11_Lang/egg.js
+++ ch11_Lang/egg.js
@@ -9,7 +9,7 @@ function parseExpression(program) {
   
   //exec returns an array of values that match the Reg Exp, or null.
   //1 - exec checks the program against the RegExp and returns an Array to the 'match' variable.
-  //2 - if match is null, then the statement is run
+  //2 - if match is null, then the next statement is run
   //3 - if match is an array with values, then expr is set. See the statement information.
   //3a - else, go to the next statement
   
@@ -65,17 +65,39 @@ console.log("Should not have leading spaces: " + skipSpace("   abv"));
 
 //===================
 
+// parseApply checks whether the expression is an application/
+// If application, then it parses a parenthesized list of arguments
+
 function parseApply(expr, program){
+	// removes any leading spaces from the 'program' argument
 	program = skipSpace(program);
+	
+	//An application has an opening parenthesis, so that condition is checked.
+	// If it is not an application, then the expr/rest structure is returned.
 	if (program[0] != "(")
 		return {expr: expr, rest: program};
 		
+	//Removes the opening parenthesis and any spaces before the next character	
 	program = skipSpace(program.slice(1));
+	
+	//updates the expr with the application structure
+	//puts the original expr argument into the operator
 	expr = {type: "apply", operator: expr, args: []};
+	
+	//iterates until the closing parenthesis
 	while (program[0] != ")"){
+		
+		//Runs parseExpression again, recursively, until the bottom of the structure is found
 		var arg = parseExpression(program);
+		
+		//adds the current data expression to the arguments array
 		expr.args.push(arg.expr);
+		
+		// removes any spaces before the 'rest' attribute
 		program = skipSpace(arg.rest);
+		
+		//It is expected that this will find a comma, so it checks for this condition.
+		// If a comma is there as expected, the values after the comma are set to the program variable.
 		if (program[0] == ",")
 			program = skipSpace(program.slice(1));
 		else if (program[0] != ")")
@@ -83,7 +105,6 @@ function parseApply(expr, program){
 	}
 	return parseApply(expr, program.slice(1));
 }
-
 
 
 //===================
@@ -99,3 +120,40 @@ function parse(program){
 
 console.log("Parse a Word: " + JSON.stringify(parse("abc")));
 console.log("Parse an Application: " + JSON.stringify(parse("+(a, 10)")));
+
+//=========================
+
+function evaluate(expr, env){
+	
+	//Checks for each expression type and performs the needed functionality
+	switch(expr.type){
+		
+		//If the expression is a value, then the value is returned.
+		case "value":
+			return expr.value;
+		
+		//If the expression is a word, then it is a variable that may or may not be defined in the environment.
+		// If the word is in the environment, then the value of the item at the index where that expression resides is returned.
+		case "word":
+			if (expr.name in env)
+				return env[expr.name];
+			else
+				throw new ReferenceError("Undefined variable: " + expr.name);
+		
+		//If the expression is an application, then there is a check if the operator type is a word.
+		case "apply":
+			
+			// if it is a word, and it is in the specialForms , then the arguments and environment are passed to that specialForms.
+			if (expr.operator.type == "word" && expr.operator.name in specialForms)
+				return specialForms[expr.operator.name](expr.args, env);
+			
+			//the operator is expected to be a function.
+			var op = evaluate(expr.operator, env);
+			if (typeof op != "function")
+				throw new TypeError("Applying a non-function");
+			return op.apply(null, expr.args.map(function(arg){
+			return evaluate(arg, env);
+			}));	
+	}
+}
+
