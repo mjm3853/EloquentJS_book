1d5d6a9381828604a8504aa878b09c7b18638b85
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-18 23:13:21 -0400

    added elt function

--- ch13/misc.js
+++ ch13/misc.js
@@ -1,18 +1,3 @@
-
-function talksAbout(node, string){
-	if (node.nodeType == document.ELEMENT_NODE){
-		for (var i=0; i < node.childNodes.length; i++){
-			if (talksAbout(node.childNodes[i], string))
-				return true;
-		}
-		return false;
-	} else if (node.nodeType == document.TEXT_NODE){
-		return node.nodeValue.indexOf(string) > -1;
-	}
-}
-
-console.log(talksAbout(document.body, "Blah"));
-
 //---------------
 
 var link = document.body.getElementsByTagName("a")[0];
@@ -35,6 +20,25 @@ document.body.insertBefore(paragraphs[1], paragraphs[0]);
 
 var button = document.getElementById("bostrichButton");
 
+//-------------------
+
+
+function talksAbout(node, string){
+	if (node.nodeType == document.ELEMENT_NODE){
+		for (var i=0; i < node.childNodes.length; i++){
+			if (talksAbout(node.childNodes[i], string))
+				return true;
+		}
+		return false;
+	} else if (node.nodeType == document.TEXT_NODE){
+		return node.nodeValue.indexOf(string) > -1;
+	}
+}
+
+console.log(talksAbout(document.body, "Blah"));
+
+//--------------------
+
 function replaceImages(){
 	var images = document.body.getElementsByTagName("img");
 		
@@ -47,16 +51,36 @@ function replaceImages(){
 	}
 }
 
+//--------------------
+
 function disableButton(){
 	button.disabled = true;
 }
+
+//-----------------------
 
 button.addEventListener("click", replaceImages, false);
 button.addEventListener("click", disableButton, false);
 
 //-------------------
 
+function elt(type){
+	var node = document.createElement(type);
+	for (i = 1; i < arguments.length; i++){
+		var child = arguments[i];
+		if (typeof child == "string")
+			child = document.createTextNode(child);
+		node.appendChild(child);
+	}
+	return node;
+}
 
+document.getElementById("quote").appendChild(
+	elt("footer", "--",
+		elt("strong", "Karl Popper"),
+		", preface to the second edition of ",
+		elt("em", "The Open Society and Its Enemies"),
+		", 1950"));
 
-
+//-------------------------
 
