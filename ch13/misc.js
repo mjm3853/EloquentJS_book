b30ea1afb8937e097e7e46743ced84ca64eb1c63
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-19 15:40:38 -0400

    add time function

--- ch13/misc.js
+++ ch13/misc.js
@@ -134,3 +134,23 @@ console.log("clientHeight:", firstParagraph.clientHeight);
 console.log("offsetHeight:", firstParagraph.offsetHeight);
 
 //------------------------
+
+function time (name, action) {
+	var start = Date.now();
+	action();
+	console.log(name, "took", Date.now() - start, "ms");
+}
+
+time("naive", function(){
+	var target = document.getElementById("one");
+	while (target.offsetWidth < 2000)
+		target.appendChild(document.createTextNode("X"));
+});
+
+time("clever", function(){
+	var target = document.getElementById("two");
+	target.appendChild(document.createTextNode("XXXXX"));
+	var total = Math.ceil(2000 / (target.offsetWidth / 5));
+	for (var i = 5; i < total; i++)
+		target.appendChild(document.createTextNode("X"));
+});
