813b360a106aaac14408451e37381b00461be1ec
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-19 09:09:14 -0400

    added formatting and highlighting functions

--- ch13/misc.js
+++ ch13/misc.js
@@ -84,3 +84,45 @@ document.getElementById("quote").appendChild(
 
 //-------------------------
 
+var paras = document.body.getElementsByTagName("p");
+Array.prototype.forEach.call(paras, function(para){
+	if (para.getAttribute("data-classified") == "secret")
+		para.parentNode.removeChild(para);
+});
+
+//----------------------------
+
+function highlightCode(node, keywords){
+	var text = node.textContent;
+	node.textContent = ""; // Clear the node
+	
+	var match, pos = 0;
+	while (match = keywords.exec(text)) {
+		var before = text.slice(pos, match.index);
+		node.appendChild(document.createTextNode(before));
+		var strong = document.createElement("strong");
+		strong.appendChild(document.createTextNode(match[0]));
+		node.appendChild(strong);
+		pos = keywords.lastIndex;
+	}
+	var after = text.slice(pos);
+	node.appendChild(document.createTextNode(after));
+}
+
+var languages = {
+	javascript: /\b(function|return|var)\b/g
+};
+
+function highlightAllCode() {
+	var pres = document.body.getElementsByTagName("pre");
+	for (var i = 0; i < pres.length; i++) {
+		var pre = pres[i];
+		var lang = pre.getAttribute("data-language");
+		if (languages.hasOwnProperty(lang))
+			highlightCode(pre, languages[lang]);
+	}
+}
+
+highlightAllCode();
+
+//-------------------------
