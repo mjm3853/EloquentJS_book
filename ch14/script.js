8fa29f90f8b4391898fbc22da3e38fa4ac262c30
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-20 16:09:02 -0400

    dots and half of dragbar

--- ch14/script.js
+++ ch14/script.js
@@ -59,3 +59,32 @@ addEventListener("keyup", function(event){
 	if (event.keyCode == 86)
 		document.body.style.background = "";
 });
+
+//--------------------------------
+
+addEventListener("click", function(event) {
+	var dot = document.createElement("div");
+	dot.className = "dot";
+	dot.style.left = (event.pageX - 4) + "px";
+	dot.style.top = (event.pageY - 4) + "px";
+	document.body.appendChild(dot);
+});
+
+//--------------------
+
+var lastX;
+var rect = document.querySelector("#dragBar");
+rect.addEventListener("mousedown", function(event){
+	if (event.which == 1){
+		lastX = event.pageX;
+		addEventListener("mousemove", moved);
+		event.preventDefault(); //Prevent Selection
+	}
+});
+
+function butttonPressed(event) {
+	if (event.buttons == null)
+		return event.which != 0;
+	else
+		return event.buttons !=0;
+}
