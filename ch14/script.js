03755b9bf45c1e75d68786e988781b5c8f9162cd
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-20 19:08:10 -0400

    added focus and blur

--- ch14/script.js
+++ ch14/script.js
@@ -126,3 +126,18 @@ addEventListener("scroll", function(){
 	var percent = (pageYOffset / max) * 100;
 	bar.style.width = percent + "%";
 });
+
+//---------------------------------
+
+var help = document.querySelector("#help");
+var fields = document.querySelectorAll("input");
+
+for (var i=0; i < fields.length; i++){
+	fields[i].addEventListener("focus", function(event){
+		var text = event.target.getAttribute("data-help");
+		help.textContent = text;
+	});
+	fields[i].addEventListener("blur", function(event){
+		help.textContent = "";
+	});
+}
