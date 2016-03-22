6c5ecedf6922a527a8c847fd1cc80fb8238e95ee
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-21 22:26:11 -0400

    finished the drawing and drew a simple level!

--- ch15/levels.js
+++ ch15/levels.js
@@ -101,7 +101,7 @@ function Lava(pos, ch){
 	}
 }
 
-Lava.prototype.type = "Lava";
+Lava.prototype.type = "lava";
 
 var testLava = new Lava(new Vector(5,5), "|");
 
@@ -115,7 +115,7 @@ function Coin(pos){
 	this.wobble = Math.random() * Math.PI * 2;
 }
 
-Coin.prototype.type = "Coin";
+Coin.prototype.type = "coin";
 
 var testCoin = new Coin(new Vector(5,5));
 
@@ -158,10 +158,68 @@ DOMDisplay.prototype.drawBackground = function(){
 		var rowElt = table.appendChild(elt("tr"));
 		rowElt.style.height = scale + "px";
 		row.forEach(function(type){
-			rowElt.appendChild(elt("td", "style"));
+			rowElt.appendChild(elt("td", type));
 		});
 	});
 	return table;
 };
 
+//--------------------------------------
+
+DOMDisplay.prototype.drawActors = function(){
+	var wrap = elt("div");
+	this.level.actors.forEach(function(actor){
+		var rect = wrap.appendChild(elt("div", "actor " + actor.type));
+		rect.style.width = actor.size.x * scale + "px";
+		rect.style.height = actor.size.y * scale + "px";
+		rect.style.left = actor.pos.x * scale + "px";
+		rect.style.top = actor.pos.y * scale + "px";
+	});
+	return wrap;
+};
+
+//----------------------------------------
+
+DOMDisplay.prototype.drawFrame = function(){
+	if(this.actorLayer)
+		this.wrap.removeChild(this.actorLayer);
+	this.actorLayer = this.wrap.appendChild(this.drawActors());
+	this.wrap.className = "game " + (this.level.status || "");
+	this.scrollPlayerIntoView();
+};
+
+//---------------------------------------
+
+DOMDisplay.prototype.scrollPlayerIntoView = function(){
+	var width = this.wrap.clientWidth;
+	var height = this.wrap.clientHeight;
+	var margin = width / 3;
+	
+	//The viewport
+	var left = this.wrap.scrollLeft, right = left + width;
+	var top = this.wrap.scrollTop, bottom = top + height;
+	
+	var player = this.level.player;
+	var center = player.pos.plus(player.size.times(0.5)).times(scale);
+	
+	if(center.x < left + margin)
+		this.wrap.scrollLeft = center.x - margin;
+	else if (center.x > right - margin)
+		this.wrap.scrollLeft = center.x + margin - width;
+	if (center.y < top + margin)
+		this.wrap.scrollTop = center.y - margin;
+	else if (center.y > bottom - margin)
+		this.wrap.scrollTop = center.y + margin - height; 
+};
+
+//--------------------------------
+
+DOMDisplay.prototype.clear = function(){
+	this.wrap.parentNode.removeChild(this.wrap);
+};
+
+//-----------------------------
+
+var simpleLevel = new Level(simpleLevelPlan);
+var display = new DOMDisplay(document.body, simpleLevel);
 
