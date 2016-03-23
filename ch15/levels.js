adeaef7332f7d5847aafe2c93a8ec044b5544c6a
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-22 20:31:35 -0400

    added player methods

--- ch15/levels.js
+++ ch15/levels.js
@@ -298,9 +298,57 @@ Coin.prototype.act = function(step){
 	this.pos = this.basePos.plus(new Vector(0, wobblePos));
 };
 
+//--------------------------------------
 
+var playerXSpeed = 7;
 
+Player.prototype.moveX = function(step, level, keys){
+	this.speed.x = 0;
+	if (keys.left) this.speed.x -= playerXSpeed;
+	if (keys.right) this.speed.x += playerXSpeed;
+	
+	var motion = new Vector(this.speed.x * step, 0);
+	var newPos = this.pos.plus(motion);
+	var obstacle = level.obstacleAt(newPos, this.size);
+	if (obstacle)
+		level.playerTouched(obstacle);
+	else
+		this.pos = newPos;
+};
 
+var gravity = 30;
+var jumpSpeed = 17;
+
+Player.prototype.moveY = function(step, level, keys){
+	this.speed.y += step * gravity;
+	var motion = new Vector(0, this.speed.y * step);
+	var newPos = this.pos.plus(motion);
+	var obstacle = level.obstacleAt(newPos, this.size);
+	if (obstacle){
+		level.playerTouched(obstacle);
+		if (keys.up && this.speed.y > 0)
+			this.speed.y = -jumpSpeed;
+		else
+			this.speed.y = 0;
+	} else {
+		this.pos = newPos;
+	}
+};
+
+Player.prototype.act = function(step, level, keys){
+	this.moveX(step, level, keys);
+	this.moveY(step, level, keys);
+	
+	var otherActor = level.actorAt(this);
+	if (otherActor)
+		level.playerTouched(otherActor.type, otherActor);
+		
+	//Losing animation
+	if (level.status == "lost") {
+		this.pos.y += step;
+		this.size.y -= step;
+	}
+};
 
 
 
