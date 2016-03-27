bb68004b38285d518ae530bf2499afcdcf865b72
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-26 23:33:30 -0400

    canvas time

---
+++ ch16/script.js
@@ -0,0 +1,29 @@
+var cx = document.querySelector("canvas").getContext("2d");
+
+/* Hide Start
+
+//draw two rectangles
+cx.strokeStyle = "blue";
+cx.strokeRect(5,5,50,50);
+cx.lineWidth = 5;
+cx.strokeRect(135,5,50,50);
+
+//draw some lines
+cx.beginPath();
+for (var y=70; y <130; y += 10){
+	cx.moveTo(10,y);
+	cx.lineTo(90,y);
+}
+cx.stroke();
+
+
+//draw triangle
+cx.beginPath();
+cx.moveTo(50,10);
+cx.lineTo(10,70);
+cx.lineTo(90,70);
+cx.fill();
+
+*/
+
+
