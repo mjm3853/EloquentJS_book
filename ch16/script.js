a210414d2440c26eecf8efa44186fef4515cf857
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-27 19:56:40 -0400

    canvas quadratic and bezier curves

--- ch16/script.js
+++ ch16/script.js
@@ -16,7 +16,6 @@ for (var y=70; y <130; y += 10){
 }
 cx.stroke();
 
-
 //draw triangle
 cx.beginPath();
 cx.moveTo(50,10);
@@ -24,6 +23,24 @@ cx.lineTo(10,70);
 cx.lineTo(90,70);
 cx.fill();
 
+//draw star trek type symbol
+cx.beginPath();
+cx.moveTo(10,90);
+//Parts of quadratic curve: control=(60,10) goal=(90,90)
+cx.quadraticCurveTo(60,10,90,90);
+cx.lineTo(60,10);
+cx.closePath();
+cx.stroke();
+
+//draw pointy pants
+cx.beginPath();
+cx.moveTo(10,90);
+//Parts of bezier curve: control1=(10,10) control2=(90,10) goal=(50,90)
+cx.bezierCurveTo(5,10,90,10,50,90);
+cx.lineTo(90,10);
+cx.lineTo(10,10);
+cx.closePath();
+cx.stroke();
+
+End Hide
 */
-
-
