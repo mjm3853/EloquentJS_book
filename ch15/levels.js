9ef9e78e3b3d1b0acebb72fb148ef1be28a4abd2
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-28 21:20:11 -0400

    ch15 updates to add a canvas display

--- ch15/levels.js
+++ ch15/levels.js
@@ -612,3 +612,42 @@ if (typeof module != "undefined" && module.exports)
 
 runGame(GAME_LEVELS, DOMDisplay);
 
+//----------------------------------------
+
+function CanvasDisplay(parent,level){
+	this.canvas = document.createElement("canvas");
+	this.canvas.width = Math.min(600, level.width * scale);
+	this.canvas.height = Math.min(450, level.height * scale);
+	parent.appendChild(this.canvas);
+	this.cx = this.canvas.getContext("2d");
+	
+	this.level = level;
+	this.animationTime = 0;
+	this.flipPlayer = false;
+	
+	this.viewPort = {
+		left: 0,
+		top: 0,
+		width: this.canvas.width / scale,
+		height: this.canvas.height / scale
+	};
+	
+	this.drawFrame(0);
+}
+
+CanvasDisplay.prototype.clear = function(){
+	this.canvas.parentNode.removeChild(this.canvas);
+};
+
+CanvasDisplay.prototype.drawFrame = function(step){
+	this.animationTime += step;
+	
+	this.updateViewport();
+	this.clearDisplay();
+	this.drawBackground();
+	this.drawActors();
+};
+
+CanvasDisplay.prototype.updateViewport = function(){
+	
+};
