0cc7aed02be8f964e4fdc6e4eb22619f24a14df9
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-03-28 21:31:57 -0400

    some canvas display methods

--- ch15/levels.js
+++ ch15/levels.js
@@ -649,5 +649,26 @@ CanvasDisplay.prototype.drawFrame = function(step){
 };
 
 CanvasDisplay.prototype.updateViewport = function(){
+	var view = this.viewPort, margin = view.width/3;
+	var player = this.level.player;
+	var center = player.pos.plus(player.size.times(0.5));
 	
+	if (center.x < view.left + margin)
+		view.left = Math.max(center.x - margin, 0);
+	else if (center.x > view.left + view.width - margin)
+		view.left = Math.min(center.x + margin - view.width, this.level.width - view.width);
+	if (center.y < view.top + margin)
+		view.top = Math.max(center.y - margin, 0);
+	else if (center.y > view.top + view.height - margin)
+		view.top = Math.min(center.y + margin - view.height, this.level.height - view.height);
+};
+
+CanvasDisplay.prototype.clearDisplay = function(){
+	if(this.level.status == "won")
+		this.cx.fillStyle = "rgb(68,191,255)";
+	else if (this.level.status == "lost")
+		this.cx.fillStyle = "rgb(44,136,214)";
+	else
+		this.cx.fillStyle = "rgb(52,166,251)";
+	this.cx.fillRect(0,0,this.canvas.width, this.canvas.height);
 };
