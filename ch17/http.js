a3ada2156d8d7f581ea134cf8ad3009fe050babd
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-01 22:48:24 -0400

    promises and a loading function. TODO: chapter exercises

--- ch17/http.js
+++ ch17/http.js
@@ -1,3 +1,4 @@
+'use strict';
 
 /*
 var req = new XMLHttpRequest();
@@ -72,9 +73,62 @@ function getURL(url, callback){
 
 //----------------------------
 
-getURL("example/data.txt", function(content, url, error){
+getURL("nonsense/data.txt", function(content, url, error){
 	if (error != null)
-		console.log("Failed to fetch " + url + " with error: " + error);
+		console.log("@getURL - Failed to fetch " + url + " with: " + error);
 	else
-		console.log("Content of " + url + ": " + content);
+		console.log("@getURL - Content of " + url + "is: " + content);
+});
+
+//-------------------------------------
+
+function get(url){
+	return new Promise(function(succeed, fail){
+		var req = new XMLHttpRequest();
+		req.open("GET", url, true);
+		req.addEventListener("load", function(){
+			if (req.status < 400)
+				succeed(req.responseText);
+			else
+				fail(new Error("Request failed: " + req.statusText));
+		});
+		req.addEventListener("error", function(){
+			fail (new Error("Network error"));
+		});
+		req.send(null);
+	});
+}
+
+/*
+get("example/data.txt").then(function(respURL){
+	console.log("@get - Succeeded at: " + respURL);	
+}, function(error){
+	console.log("@get - Failed with: " + error);
+});
+*/
+
+//-----------------------------------------
+
+function getJSON(url){
+	return get(url).then(JSON.parse);
+}
+
+function showMessage(msg){
+	var elt = document.createElement("div");
+	elt.textContent = msg;
+	return document.body.appendChild(elt);
+}
+
+var loading = showMessage("Loading...");
+
+getJSON("example/bert.json").then(function(bert){
+	return getJSON(bert.spouse);
+}).then(function(spouse){
+	return getJSON(spouse.mother);
+}).then(function(mother){
+	showMessage("The name is " + mother.name);
+}).catch(function(error){
+	showMessage(String(error));
+}).then(function(){
+	document.body.removeChild(loading);
 });
