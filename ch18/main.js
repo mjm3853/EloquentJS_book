8548d64ccc0d32f860f0a2d44098c014c0becd14
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-03 21:50:25 -0400

    ch18 more stuf

--- ch18/main.js
+++ ch18/main.js
@@ -34,3 +34,47 @@ var output = document.querySelector("#length");
 text.addEventListener("input", function(){
 	output.textContent = text.value.length;
 });
+
+//---------------------------------
+
+var checkbox = document.querySelector("#purple");
+checkbox.addEventListener("change", function(){
+	document.body.style.background = checkbox.checked ? "mediumpurple" : "";
+});
+
+//---------------------------------
+
+var buttons = document.getElementsByName("color");
+
+function setColor(event){
+	document.body.style.background = event.target.value;
+}
+
+for (var i = 0; i < buttons.length; i++)
+	buttons[i].addEventListener("change", setColor);
+
+//--------------------------------------
+
+var select = document.querySelector("select");
+var output = document.querySelector("#output");
+select.addEventListener("change", function(){
+	var number = 0;
+	for (var i = 0; i < select.options.length; i++){
+		var option = select.options[i];
+		if (option.selected)
+			number += Number(option.value);
+	}
+	output.textContent = number;
+});
+
+//----------------------------------------
+
+var input = document.querySelector("#fileInput");
+input.addEventListener("change", function(){
+	if (input.files.length > 0) {
+		var file = input.files[0];
+		console.log("You chose", file.name);
+		if(file.type)
+			console.log("It has type", file.type);
+	}
+});
