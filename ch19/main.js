09b572baadde6ba8c05a3df002c6812d68fb2335
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-05 22:10:57 -0400

    text tool

--- ch19/main.js
+++ ch19/main.js
@@ -187,4 +187,16 @@ controls.openURL = function(cx){
 
 //--------------------------------------------------
 
+tools.Text = function(event, cx){
+	var text = prompt("Text: ", "");
+	if (text){
+		var pos = relativePos(event, cx.canvas);
+		cx.font = Math.max(7, cx.lineWidth) + "px sans-serif";
+		cx.fillText(text, pos.x, pos.y);
+	}
+		
+};
+
+//------------------------------------------
+
 createPaint(document.body);
