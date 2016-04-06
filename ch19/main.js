e49917e8d66ca747564956d010f32876b4eedee7
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-05 21:36:25 -0400

    save tool

--- ch19/main.js
+++ ch19/main.js
@@ -123,4 +123,23 @@ controls.brushSize = function(cx){
 
 //------------------------------------------
 
+controls.save = function(cx){
+	var link = elt("a", {href: "/"}, "Save");
+	function update(){
+		try {
+			link.href = cx.canvas.toDataURL();
+		} catch(e) {
+			if (e instanceof SecurityError)
+				link.href = "javascript:alert(" + JSON.stringify("Can't save: " + e.toString()) + ")";
+			else
+				throw e;
+		}
+	}
+	link.addEventListener("mouseover", update);
+	link.addEventListener("focus", update);
+	return link;
+};
+
+//--------------------------------------------
+
 createPaint(document.body);
