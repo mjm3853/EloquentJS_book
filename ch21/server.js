67d2e1ab369cc77e6ce59522fd030ca5c3b48564
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-10 12:06:50 -0400

    read stream as JSON function

--- ch21/server.js
+++ ch21/server.js
@@ -39,3 +39,23 @@ router.add("DELETE", /^\/talks\/([^\/]+)$/, function(request, response, title){
 	}
 	respond(response, 204, null);
 });
+
+//-----------------------------
+
+function readStreamAsJSON(stream, callback){
+	var data = "";
+	stream.on("data", function(chunk){
+		data += chunk;
+	});
+	stream.on("end", function(){
+		var result, error;
+		try { result = JSON.parse(data); }
+		catch (e) { error = e; }
+		callback(error, result);
+	});
+	stream.on("error", function(error){
+		callback(error);
+	});
+}
+
+//------------------------------------
