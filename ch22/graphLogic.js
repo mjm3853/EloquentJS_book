47ecb5750efaa7f7a108b3a7677e1208121efde9
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-10 21:50:52 -0400

    simple graph drawn without bugs

--- ch22/graphLogic.js
+++ ch22/graphLogic.js
@@ -37,4 +37,36 @@ var repulsionStrength = 1500;
 
 //-------------------------------
 
+function forceDirected_simple(graph){
+	graph.forEach(function(node){
+		graph.forEach(function(other){
+			if (other == node) return;
+			var apart = other.pos.minus(node.pos);
+			var distance = Math.max(1, apart.length);
+			var forceSize = -repulsionStrength / (distance * distance);
+			if (node.hasEdge(other))
+				forceSize += (distance - springLength) * springStrength;
+			var normalized = apart.times(1/distance);
+			node.pos = node.pos.plus(normalized.times(forceSize));
+		});
+	});
+}
 
+//--------------------------------
+
+function runLayout(implementation, graph){
+	var totalSteps = 0, time = 0;
+	function step(){
+		var startTime = Date.now();
+		for (var i = 0; i < 100; i++)
+			implementation(graph);
+		totalSteps += 100;
+		time += Date.now() - startTime;
+		drawGraph(graph);
+		if (totalSteps < 4000)
+			requestAnimationFrame(step);
+		else
+			console.log(time);
+	}
+	step();
+}
